015503fb15339ed24b020126b9159339acc94db0
xu.qinqin <xu.qinqin@danatech.com>
2021-05-20 19:41:35 +0800

    leetcode: array easy

---
+++ src/test/java/com/exp/leetcode/array/easy/FindCommonCharactersTest.java
@@ -0,0 +1,29 @@
+package com.exp.leetcode.array.easy;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FindCommonCharactersTest {
+  @Test
+  void test1() {
+    final FindCommonCharacters findCommonCharacters = new FindCommonCharacters();
+
+    String[] words = {"bella", "label", "roller"};
+
+    List<String> expected = List.of("e", "l", "l");
+
+    Assertions.assertEquals(expected, findCommonCharacters.commonChars(words));
+  }
+
+  @Test
+  void test2() {
+    final FindCommonCharacters findCommonCharacters = new FindCommonCharacters();
+
+    String[] words = {"cool", "lock", "cook"};
+
+    List<String> expected = List.of("c", "o");
+
+    Assertions.assertEquals(expected, findCommonCharacters.commonChars(words));
+  }
+}
