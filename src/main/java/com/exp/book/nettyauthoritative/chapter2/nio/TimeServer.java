bd65ea464ad7a02f89488f4006c0cfe3dc2d9ded
xu.qinqin <xu.qinqin@danatech.com>
2021-05-31 21:04:52 +0800

    nio netty

--- src/main/java/com/exp/book/nettyauthoritative/chapter2/nio/TimeServer.java
+++ src/main/java/com/exp/book/nettyauthoritative/chapter2/nio/TimeServer.java
@@ -1,11 +1,15 @@
 package com.exp.book.nettyauthoritative.chapter2.nio;
 
+import com.google.common.base.Strings;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.Set;
 import lombok.SneakyThrows;
 
@@ -17,10 +21,13 @@ public class TimeServer {
     }
 
     final MultiplexerTimeServer multiplexerTimeServer = new MultiplexerTimeServer(port);
+    new Thread(multiplexerTimeServer, "NIO-TimeServer-001").start();
   }
 }
 
 class MultiplexerTimeServer implements Runnable {
+  private static final int backlog = 1024;
+
   private Selector selector;
   private ServerSocketChannel serverSocketChannel;
   private volatile boolean stop = false;
@@ -31,7 +38,7 @@ class MultiplexerTimeServer implements Runnable {
       selector = Selector.open();
       serverSocketChannel = ServerSocketChannel.open();
       serverSocketChannel.configureBlocking(false);
-      serverSocketChannel.socket().bind(new InetSocketAddress(port), 1024);
+      serverSocketChannel.socket().bind(new InetSocketAddress(port), backlog);
       serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
       System.out.println("time server is start in port: " + port);
     } catch (IOException e) {
@@ -80,7 +87,34 @@ class MultiplexerTimeServer implements Runnable {
         socketChannel.configureBlocking(false);
         socketChannel.register(selector, SelectionKey.OP_READ);
       }
-      if (key.isReadable()) {}
+      if (key.isReadable()) {
+        final SocketChannel socketChannel = (SocketChannel) key.channel();
+        final ByteBuffer readBuffer = ByteBuffer.allocate(backlog);
+        int readBytes = socketChannel.read(readBuffer);
+        if (readBytes > 0) {
+          readBuffer.flip();
+          final byte[] bytes = new byte[readBuffer.remaining()];
+          readBuffer.get(bytes);
+          final String body = new String(bytes, StandardCharsets.UTF_8);
+          System.out.println("time server receive order: " + body);
+          doWrite(
+              socketChannel,
+              "query time order".equals(body) ? Instant.now().toString() : "bad order");
+        } else if (readBytes < 0) {
+          key.cancel();
+          socketChannel.close();
+        }
+      }
+    }
+  }
+
+  private void doWrite(SocketChannel socketChannel, String response) throws IOException {
+    if (Strings.isNullOrEmpty(response)) {
+      final byte[] bytes = response.getBytes();
+      final ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+      writeBuffer.put(bytes);
+      writeBuffer.flip();
+      socketChannel.write(writeBuffer);
     }
   }
 }
