42f986ecd5ecb517940acd6ead42a9dc7126d527
xu.qinqin <xu.qinqin@danatech.com>
2021-06-09 18:53:22 +0800

    string medium

---
+++ src/main/java/com/exp/leetcode/string/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,33 @@
+package com.exp.leetcode.string.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * <p>来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ *
+ * @number 3
+ * @date 2021/6/9 15:52
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+  public int lengthOfLongestSubstring(String s) {
+    Map<Character, Integer> map = new HashMap<>();
+
+    int max = 0;
+    int i = 0;
+    for (; i < s.length(); i++) {
+      final char ch = s.charAt(i);
+      if (map.containsKey(ch)) {
+        max = Math.max(max, map.size());
+        i = map.get(ch);
+        map.clear();
+      } else {
+        map.put(ch, i);
+      }
+    }
+    return Math.max(max, map.size());
+  }
+}
---
+++ src/test/java/com/exp/leetcode/string/easy/FirstUniqueCharacterInAStringTest.java
@@ -0,0 +1,42 @@
+package com.exp.leetcode.string.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstUniqueCharacterInAStringTest {
+  @Test
+  void test1() {
+    final FirstUniqueCharacterInAString firstUniqueCharacterInAString =
+        new FirstUniqueCharacterInAString();
+
+    String s = "leetcode";
+
+    int expected = 0;
+
+    Assertions.assertEquals(expected, firstUniqueCharacterInAString.firstUniqChar(s));
+  }
+
+  @Test
+  void test2() {
+    final FirstUniqueCharacterInAString firstUniqueCharacterInAString =
+        new FirstUniqueCharacterInAString();
+
+    String s = "loveleetcode";
+
+    int expected = 2;
+
+    Assertions.assertEquals(expected, firstUniqueCharacterInAString.firstUniqChar(s));
+  }
+
+  @Test
+  void test3() {
+    final FirstUniqueCharacterInAString firstUniqueCharacterInAString =
+        new FirstUniqueCharacterInAString();
+
+    String s = "aabb";
+
+    int expected = -1;
+
+    Assertions.assertEquals(expected, firstUniqueCharacterInAString.firstUniqChar(s));
+  }
+}
