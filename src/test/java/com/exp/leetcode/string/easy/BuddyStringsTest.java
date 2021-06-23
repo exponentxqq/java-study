c15fc1d050f8bad77f5ae7d28ff2fdc409fa9ed3
xu.qinqin <xu.qinqin@danatech.com>
2021-06-23 14:22:38 +0800

    string easy

---
+++ src/test/java/com/exp/leetcode/string/easy/BuddyStringsTest.java
@@ -0,0 +1,78 @@
+package com.exp.leetcode.string.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BuddyStringsTest {
+  @Test
+  void test1() {
+    final BuddyStrings buddyStrings = new BuddyStrings();
+
+    String s = "ab";
+    String goal = "ba";
+
+    boolean expected = true;
+
+    Assertions.assertEquals(expected, buddyStrings.buddyStrings(s, goal));
+  }
+
+  @Test
+  void test2() {
+    final BuddyStrings buddyStrings = new BuddyStrings();
+
+    String s = "ab";
+    String goal = "ab";
+
+    boolean expected = false;
+
+    Assertions.assertEquals(expected, buddyStrings.buddyStrings(s, goal));
+  }
+
+  @Test
+  void test3() {
+    final BuddyStrings buddyStrings = new BuddyStrings();
+
+    String s = "aa";
+    String goal = "aa";
+
+    boolean expected = true;
+
+    Assertions.assertEquals(expected, buddyStrings.buddyStrings(s, goal));
+  }
+
+  @Test
+  void test4() {
+    final BuddyStrings buddyStrings = new BuddyStrings();
+
+    String s = "";
+    String goal = "aa";
+
+    boolean expected = false;
+
+    Assertions.assertEquals(expected, buddyStrings.buddyStrings(s, goal));
+  }
+
+  @Test
+  void test5() {
+    final BuddyStrings buddyStrings = new BuddyStrings();
+
+    String s = "aaaaaaabc";
+    String goal = "aaaaaaacb";
+
+    boolean expected = true;
+
+    Assertions.assertEquals(expected, buddyStrings.buddyStrings(s, goal));
+  }
+
+  @Test
+  void test6() {
+    final BuddyStrings buddyStrings = new BuddyStrings();
+
+    String s = "abab";
+    String goal = "abab";
+
+    boolean expected = true;
+
+    Assertions.assertEquals(expected, buddyStrings.buddyStrings(s, goal));
+  }
+}
