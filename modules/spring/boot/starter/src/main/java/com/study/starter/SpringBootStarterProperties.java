68b99e675c0b4d5b7d934f170047bcac4921ee65
xu.qinqin <xu.qinqin@danatech.com>
2021-08-13 13:15:31 +0800

    spring boot starter properties hits

--- modules/spring/boot/starter/src/main/java/com/study/starter/SpringBootStarterProperties.java
+++ modules/spring/boot/starter/src/main/java/com/study/starter/SpringBootStarterProperties.java
@@ -4,9 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "starter")
+@ConfigurationProperties(prefix = SpringBootStarterProperties.PREFIX)
 @Setter
 @Getter
 public class SpringBootStarterProperties {
+  public static final String PREFIX = "starter";
   private String studentName = "autoconfig";
 }
